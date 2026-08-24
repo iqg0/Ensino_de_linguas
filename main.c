bb25277575e3eb52aaa70698b24ec968027362f2
Iago <iagocamargo95@gmail.com>
2026-08-24 15:22:29 -0300

    Teste structs para o sistema

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+    typedef struct {
+        char nome[50];
+        int id;
+    }Aluno;
+
+    typedef struct {
+        int idAluno;
+        float nota;
+        int idTurma;
+    }Matricula;
+
+    typedef struct {
+        Matricula alunos[30];
+        int serie;
+        char professorNome[50]; 
+        char horarios[10];
+        char dias[20];
+        int maxLugares;
+        int idCurso;
+        int totalAlunos;
+    }Turma;
+
+
 int main() {
+
+
 
 
     return 0;
